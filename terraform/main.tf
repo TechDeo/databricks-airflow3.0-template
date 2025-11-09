310a6c8b25cf668a5d74bb3cb0cb313da8061943
Adeola Oladeji <adeola.oladeji@axiomlaw.com>
2025-11-09 17:29:48 +0000

    ec2 docker compose

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,7 +154,7 @@ resource "aws_instance" "airflow" {
   vpc_security_group_ids = [aws_security_group.airflow.id]
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
